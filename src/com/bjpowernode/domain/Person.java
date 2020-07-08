8d2b931d80f760d956c4135d2e2cb4c0671e4ab9
wb <wangbu_scu@163.com>
2020-07-08 15:45:06 +0800

    123

---
+++ src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.domain;
+
+/**
+ * @PackgeName: com.bjpowernode.domain
+ * @ClassName: Person
+ * @Author: wangb
+ * Date: 2020/7/8 15:37
+ * project name: wrokspaceForGit01
+ * @Version:
+ * @Description:
+ */
+public class Person {
+
+    private String id;
+}
